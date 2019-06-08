fe16e9edf8129bb96e485d59a6b40b85a21754ba
react-nodejs-tutor <sualyer010@snu.ac.kr>
2019-06-08 15:33:38 +0900

    course materials

--- src/App.js
+++ src/App.js
@@ -1,26 +1,27 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import { bindActionCreators } from 'redux';
+import { connect } from 'react-redux';
+import * as counterActions from './modules/counter';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+	render() {
+		const { CounterActions, number } = this.props;
+
+		return (
+			<div>
+				<h1>{number}</h1>
+				<button onClick={CounterActions.increment}>+</button>
+				<button onClick={CounterActions.decrement}>-</button>
+			</div>
+		);
+	}
 }
 
-export default App;
+export default connect(
+	state => ({
+		number: state.counter
+	}),
+	dispatch => ({
+		CounterActions: bindActionCreators(counterActions, dispatch)
+	})
+)(App);
